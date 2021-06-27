bec466dc1c8b39837a1b3092172d6f3dac604354
Andrii Strynzha <a.strynzha@gmail.com>
2021-06-27 18:21:31 +0200

    Frontend last state

--- lib/object-page.dart
+++ lib/object-page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:frontend/stars.dart';
 import 'package:flutter/foundation.dart';
 
-
 class ObjectPage extends StatelessWidget {
   String value;
   ObjectPage({required this.value});
@@ -13,29 +12,57 @@ class ObjectPage extends StatelessWidget {
   String testText = "testText";
 
   String description =
-      "Sammelname für Legierungen, die neben Kupfer (ca. 60 %) überwiegend aus Zinn bestehen. Bronze wurde bereits in vorgeschichtlicher Zeit (Bronzezeit) für Geräte, Plastiken, Schmuck und Waffen verwendet. Ab einem Anteil von 10 % Zinn ist die gute Gießbarkeit der Bronze gewährleistet, es verringert sich aber die Elastizität und die Schmiedbarkeit. Gute geschmiedete Bronze-Klingen enthalten meist nicht mehr als 4 % Zinn. Bei höherem Zinnanteil wurde schon in der Bronzezeit Arsen als Legierungsbestandteil beigemengt, um die Härte zu erhöhen.";
+      "The Last Supper portrays the reaction given by each apostle when Jesus said one of them would betray him. All twelve apostles have different reactions to the news, with various degrees of anger and shock. The apostles were identified by their names, using an unsigned, mid-sixteenth-century fresco copy of Leonardo's Cenacolo.";
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
         body: SingleChildScrollView(
             child: Column(
                 mainAxisSize: MainAxisSize.min,
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  _image(),
-                  _description(),
-                  star1,
-                  star2,
-                  star3,
-                  ElevatedButton(
-                    onPressed: () {
-                      debugPrint("Ranking changed: " + star1.getRating().toString());
-                      star1.getRating();
-                    },
-                    child: Text('Submit'),
-                  ),
-                ])));
+          _image(),
+          _description(),
+          Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+            Row(children: [
+              star1,
+              Text('  Joy'),
+            ]),
+            Row(children: [
+              star2,
+              Text('  Empathy'),
+            ]),
+            Row(children: [
+              star3,
+              Text('  Thoughtfulness'),
+            ]),
+          ]),
+          ElevatedButton(
+            onPressed: () {
+              debugPrint("Ranking changed: " + star1.getRating().toString());
+              star1.getRating();
+            },
+            child: Text('Submit'),
+          ),
+          Text(
+            'Recommendations:',
+            style: TextStyle(fontSize: 18),
+          ),
+          SizedBox(
+            height: 200.0,
+            child: ListView.builder(
+              physics: ClampingScrollPhysics(),
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: 15,
+              itemBuilder: (BuildContext context, int index) => Card(
+                child: Center(child: Text('Recommendation card')),
+              ),
+            ),
+          ),
+        ])));
   }
 
   Widget _rankings() {
@@ -45,7 +72,7 @@ class ObjectPage extends StatelessWidget {
   Container _image() {
     return new Container(
       color: Colors.grey[200],
-      child: new Image.asset('assets/MK_4991.jpg'),
+      child: new Image.asset('assets/MK_1000.jpg'),
       alignment: Alignment.center,
     );
   }
